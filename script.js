69b5f67377c65e84f50e07043ccfe40838551ede
ZoeCarmonaGlia <mariana.zoecarmonaayala@glia.com>
2026-07-16 08:11:22 -0600

    Adding a testing modal

--- script.js
+++ script.js
@@ -26,9 +26,31 @@ if (loginForm) {
 
 // Dashbpard
 const logoutBtn = document.getElementById("logoutBtn");
+const logoutModal = document.getElementById('logoutModal');
+const cancelBtn = document.getElementById('cancelBtn');
+const confirmBtn = document.getElementById('confirmBtn');
 
 if (logoutBtn) {
-    logoutBtn.addEventListener("click", function() {
+    logoutBtn.addEventListener("click", function(e) {
+        e.preventDefault(); // Prevents any default button behavior
+        logoutModal.classList.add('active');
+    });
+}
+
+if (cancelBtn) {
+    cancelBtn.addEventListener("click", function() {
+        logoutModal.classList.remove('active');
+    });
+}
+
+window.addEventListener("click", function(event) {
+    if (event.target === logoutModal) {
+        logoutModal.classList.remove('active');
+    }
+});
+
+if (confirmBtn) {
+    confirmBtn.addEventListener("click", function() {
         localStorage.removeItem("ceGliaBankLoggedIn");
         window.location.href = "login.html";
     });
